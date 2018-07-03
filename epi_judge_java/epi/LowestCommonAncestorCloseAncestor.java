e8e8cb71bf2d10c803ca1944988c7d505cebfd57
Christopher M Ray <christopher.michael.ray@ibm.com>
2018-07-03 05:30:39 -0700

    12.4 revisit, O(n)

--- epi_judge_java/epi/LowestCommonAncestorCloseAncestor.java
+++ epi_judge_java/epi/LowestCommonAncestorCloseAncestor.java
@@ -1,4 +1,6 @@
 package epi;
+import java.util.HashSet;
+
 import epi.test_framework.BinaryTreeUtils;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
@@ -8,8 +10,35 @@ public class LowestCommonAncestorCloseAncestor {
 
   public static BinaryTree<Integer> LCA(BinaryTree<Integer> node0,
                                         BinaryTree<Integer> node1) {
-    // TODO - you fill in here.
-    return null;
+	  
+	  //if ancestor of itself
+	  if(node1.equals(node0)) {
+		  return node1;
+	  }
+   
+	  HashSet<BinaryTree<Integer>> visited = new HashSet<>();
+	  while(node0 != null || node1!= null) {
+		  if(node0 != null) {
+			  if(visited.contains(node0)) {
+				  return node0;
+			  }
+			  else {
+				  visited.add(node0);
+				  node0 = node0.parent;
+			  }
+		  }
+		  if(node1 != null) {
+			  if(visited.contains(node1)) {
+				  return node1;
+			  }
+			  else {
+				  visited.add(node1);
+				  node1 = node1.parent;
+			  }
+		  }
+		  
+	  }
+	  return node1;
   }
   @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
   public static int lcaWrapper(TimedExecutor executor, BinaryTree<Integer> tree,
