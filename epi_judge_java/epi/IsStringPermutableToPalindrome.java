79f78490776b1cb17832d9185fc925a1373d4e8c
Christopher M Ray <christopher.michael.ray@ibm.com>
2018-07-03 04:42:14 -0700

    12.1 done O(n) time

--- epi_judge_java/epi/IsStringPermutableToPalindrome.java
+++ epi_judge_java/epi/IsStringPermutableToPalindrome.java
@@ -1,12 +1,42 @@
 package epi;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class IsStringPermutableToPalindrome {
   @EpiTest(testDataFile = "is_string_permutable_to_palindrome.tsv")
 
   public static boolean canFormPalindrome(String s) {
-    // TODO - you fill in here.
-    return true;
+    
+    Map<Character, Integer> paired = new HashMap<Character, Integer>();
+    Boolean has_middle_value = false;
+    
+    for(Character c : s.toCharArray()) {
+    	int count = paired.getOrDefault(c, 0);
+    	count++;
+    	paired.put(c, count);
+    }
+    for(Integer num : paired.values()) {
+    	System.out.println("num "+ num);
+    	if(num % 2 != 0) {
+    		System.out.println("has_middle_value");
+    		if(has_middle_value) {
+    			
+    			return false;
+    		}
+    		else
+    			has_middle_value = true;
+    	}
+    }
+    if(s.length() % 2 == 0) {
+    	return !has_middle_value;
+    }
+    else {
+    	return has_middle_value;
+    }
   }
 
   public static void main(String[] args) {
